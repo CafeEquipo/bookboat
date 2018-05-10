b4464908b7c122ef91c03f3bcf22afe4c86c9fc6
philipp <philippkynast@gmx.de>
2018-05-10 22:12:43 +0200

    HOST

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const App = require('./app');
 require('http').globalAgent.maxSockets = 5;
 
 const port = 3000;
+const HOST = '0.0.0.0';
 
 const app = new App().app;
 
-app.listen(port,'localhost',(err) => {  
+app.listen(port,HOST,(err) => {  
   if (err) {
     return console.log(err)
   }
