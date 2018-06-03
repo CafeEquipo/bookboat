b25b67292ed1e3a547b0db0d7ccc1d06f3418166
PhlppKnst <philippkynast@gmx.de>
2018-06-03 16:02:58 +0200

    angular start

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,BrowserAnimationsModule,FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
